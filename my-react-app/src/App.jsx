06860034d6239f21055cbeaaa88b875d707fde30
b.alvesmartins495 <b.alvesmartins495@mybvc.ca>
2025-10-12 17:42:03 -0600

    Modified Files: 1. src/components/App.jsx - Add route to login, adminDashboard and studentdashboard 2. src/components/Login - Created login components and added local logic for login
    Created Files:
    1. src/styles/Login.css - Add style for login page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Card from './components/ui/Card';
+//import Card from './components/ui/Card';
+import Login from './components/Login';
 import Signup from './components/Signup'; 
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/adminDashboard" element={<h2>Admin Dashboard - Coming Soon</h2>} />
+            <Route path="/studentDashboard" element={<h2>Student Dashboard - Coming Soon</h2>} />
               {/* <div className="container text-center">
                 <Card className="p-3" style={{ maxWidth: '400px', margin: '0 auto' }}>
                   <h2 style={{ color: 'var(--primary-color)' }}>Sign Up</h2>
@@ -22,14 +26,14 @@ function App() {
                 </Card>
               </div> */}
           
-            <Route path="/login" element={
+            {/* { <Route path="/login" element={
               <div className="container text-center">
                 <Card className="p-3" style={{ maxWidth: '400px', margin: '0 auto' }}>
                   <h2 style={{ color: 'var(--primary-color)' }}>Login</h2>
                   <p style={{ color: 'var(--text-light)' }}>Login page coming soon...</p>
                 </Card>
               </div>
-            } />
+            } /> */}
           </Routes>
         </main>
 
